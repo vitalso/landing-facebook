fb24cf6dd9def9c3c974b7874e018ea23dbb94be
vitalso <pro_web_evo@mail.ru>
2017-02-07 13:29:29 +0200

    Alpha public version landing

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,11 @@
       slidesToScroll: 1,
       focusOnSelect: false,
       dots: false,
-      arrows: true,
-      nextArrow: '<button class="slick-next"><i class="fa fa-angle-right fa-2x"></i></button>',
-  	  prevArrow: '<button class="slick-prev"><i class="fa fa-angle-left fa-2x"></i></button>',
+      arrows: false,
+      /*nextArrow: '<button class="slick-next"><i class="fa fa-angle-right fa-2x"></i></button>',
+  	  prevArrow: '<button class="slick-prev"><i class="fa fa-angle-left fa-2x"></i></button>',*/
+  	  autoplay: true,
+  	  autoplaySpeed: 3000
   });
 
 });
